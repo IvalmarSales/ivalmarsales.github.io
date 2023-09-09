5eea80a25989910c213e4c6f970549a9797389cd
Ivalmar Sales da Silva <ivalmarsalesdasilva@gmail.com>
2023-09-09 02:11:22 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewerport" content="width=device-width, initial-scale">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>Projeto Linkeetree</title>
+	</head>
+	<body>
+		
+	</body>
+</html>
